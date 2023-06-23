9dfcdf5ea6dbc91a70693cac30adaaf4fd3b77e5
Rodrigo de Lima Alves <rodrigolima.alves21@gmail.com>
2023-06-23 15:54:12 -0300

    first steps with PDO

--- DynamicWebApplications/website/index.php
+++ DynamicWebApplications/website/index.php
@@ -1,4 +1,13 @@
 <?php
 
 require 'functions.php';
-require 'router.php';
+//require 'router.php';
+$dsn = 'mysql:host=127.0.0.1;dbname=myapp;user=root;charset=utf8';
+$pdo = new PDO($dsn);
+
+$statement = $pdo->prepare("SELECT * FROM posts");
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+dd($posts);
